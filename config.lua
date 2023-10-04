c87f7a8e4fa128f9e41312e54b943e8a8ea0c81d
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2023-10-04 14:31:20 -0300

    first commit lvim config

---
+++ config.lua
@@ -0,0 +1,186 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.spell = false
+vim.opt.signcolumn = "auto"
+vim.opt.wrap = false
+
+vim.g.autoformat_enabled = true
+vim.g.cmp_enabled = true
+vim.g.autopairs_enabled = true
+vim.g.diagnostics_mode = 3
+vim.g.icons_enabled = true
+vim.g.ui_notifications_enabled = true
+
+-- X closes a buffer
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
+-- Centers cursor when moving 1/2 page down
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+
+-- Move buffers
+lvim.keys.normal_mode["<S-l>"] = ":bnext<cr>"
+lvim.keys.normal_mode["<S-h>"] = ":bprev<cr>"
+
+-- ["gb"] = { "<C-o>", desc = "Go back to previous cursor position" },
+-- ["gf"] = { "<C-O>", desc = "Go back to previous cursor position" },
+lvim.keys.normal_mode["gb"] = "<C-o>"
+lvim.keys.normal_mode["gf"] = "<C-O>"
+
+-- Go to definition
+lvim.keys.normal_mode["gd"] = "<cmd>lua vim.lsp.buf.definition()<CR>"
+
+-- Autorun commands
+-- ["<Leader>Ac"] = { ":AutoRunnerAddCommand<cr>", desc = "Add command to run" },
+-- ["<Leader>Ar"] = { ":AutoRunnerRun<cr>", desc = "Run command registered" },
+lvim.keys.normal_mode["<Leader>Ac"] = ":AutoRunnerAddCommand<cr>"
+lvim.keys.normal_mode["<Leader>Ar"] = ":AutoRunnerRun<cr>"
+
+lvim.builtin.which_key.setup.triggers = "auto"
+
+lvim.builtin.which_key.mappings.t = {
+  name = "+Testing",
+  t = { "<cmd>TestNearest<cr>", "Test nearest" },
+  f = { "<cmd>TestFile<cr>", "Test file" },
+  s = { "<cmd>TestSuite<cr>", "Test suite" },
+}
+lvim.builtin.which_key.mappings[";"] = { "<cmd>ToggleTerm<cr>", "Toggle terminal" }
+
+lvim.plugins = {
+  {
+    "stevearc/dressing.nvim",
+    config = function()
+      require("dressing").setup({
+        input = { enabled = false },
+      })
+    end,
+  },
+  { "nvim-neorg/neorg", ft = "norg", -- lazy-load on filetype
+    config = true, -- run require("neorg").setup()
+  },
+  {
+    "felipeagc/fleet-theme-nvim",
+    name = "fleet",
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "vim-test/vim-test",
+    lazy = false,
+    config = function()
+      vim.g["test#preserve_screen"] = false
+      vim.g["test#python#runner"] = "pytest"
+
+    end,
+  },
+  {
+		"github/copilot.vim",
+		event = "VeryLazy",
+		config = function()
+			-- copilot assume mapped
+			vim.g.copilot_assume_mapped = true
+			vim.g.copilot_no_tab_map = true
+		end,
+	},
+	{
+		"hrsh7th/cmp-copilot",
+		config = function()
+			lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
+			table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
+		end,
+	},
+  {
+  "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      -- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      -- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "1.*",
+    config = function()
+      require("window-picker").setup({
+        autoselect_one = true,
+        include_current = false,
+        filter_rules = {
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
+
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { "terminal" },
+          },
+        },
+        other_win_hl_color = "#e35e4f",
+      })
+    end,
+ },
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd "highlight default link gitblame SpecialComment"
+      require("gitblame").setup { enabled = false }
+    end,
+  },
+  {
+    "krshrimali/nvim-autorunner",
+    lazy = false,
+  },
+}
+
+-- Copilot config
+local ok, copilot = pcall(require, "copilot")
+if not ok then
+  return
+end
+
+copilot.setup {
+  suggestion = {
+    keymap = {
+      accept = "<c-l>",
+      next = "<c-j>",
+      prev = "<c-k>",
+      dismiss = "<c-h>",
+    },
+  },
+}
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+
+-- LSP mappings
+lvim.lsp.buffer_mappings.normal_mode['H'] = { vim.lsp.buf.hover, "Show documentation" }
+
+-- Window picker
+local picker = require('window-picker')
+
+vim.keymap.set("n", ",w", function()
+  local picked_window_id = picker.pick_window({
+    include_current_win = true
+  }) or vim.api.nvim_get_current_win()
+  vim.api.nvim_set_current_win(picked_window_id)
+end, { desc = "Pick a window" })
+
+-- Swap two windows using the awesome window picker
+local function swap_windows()
+  local window = picker.pick_window({
+    include_current_win = false
+  })
+  local target_buffer = vim.fn.winbufnr(window)
+  -- Set the target window to contain current buffer
+  vim.api.nvim_win_set_buf(window, 0)
+  -- Set current window to contain target buffer
+  vim.api.nvim_win_set_buf(0, target_buffer)
+end
+
+vim.keymap.set('n', ',W', swap_windows, { desc = 'Swap windows' })
+
+-- Toggle terminal
+lvim.builtin.which_key.mappings[";"] = { "<cmd>ToggleTerm<cr>", "Toggle terminal" }
